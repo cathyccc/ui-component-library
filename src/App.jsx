0833e8f32f0da42f30f144e2ef1da21fdf112e1b
cathyccc <cathyky.chan@gmail.com>
2023-10-02 12:02:54 -0400

    removed white border around body, added typography, styled primary and primary-white buttons, added options of different button types

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,109 @@
 import './App.css'
 import Button from "./components/Button/Button"
+import {LuPlus} from "react-icons/lu"
 
 function App() {
   return (
     <>
-      <h1>UI Components</h1>
-      <Button variant="primary" type="icon" onClick={() => console.log("clicked")}>
-        Button
-      </Button>
+      <h3>UI Components</h3>
+      <h5>Button</h5>
+      <section>
+        <div className="section-item">
+          <h6>Primary</h6>
+          <div>
+            <span className="btn-container">
+              <p className="btn-label">Label</p>
+              <Button variant="primary" type="label" onClick={() => console.log("clicked")}>
+                Submit
+              </Button>
+            </span>
+
+            <span className="btn-container">
+              <p className="btn-label">Disabled</p>
+              <Button variant="primary" type="label" disabled>
+                Submit
+              </Button>
+            </span>
+
+            <span className="btn-container">
+              <p className="btn-label">Label Icon</p>
+              <Button variant="primary" type="label-icon">
+                <LuPlus/>
+                Add Item
+              </Button>
+            </span>
+          </div>
+
+          <div>
+            <span className="btn-container">
+              <p className="btn-label">Icon</p>
+              <Button variant="primary" type="icon">
+                <LuPlus/>
+              </Button>
+            </span>
+
+            <span className="btn-container">
+              <p className="btn-label">Icon Circle</p>
+              <Button variant="primary" type="icon-circle">
+                <LuPlus/>
+              </Button>
+            </span>
+
+            <span className="btn-container">
+              <p className="btn-label">Loader</p>
+              <Button variant="primary" type="label" loader={true}/>
+            </span>
+          </div>
+        </div>
+
+        <div className="section-item purple white">
+          <h6>Primary-White</h6>
+          <div>
+            <span className="btn-container">
+              <p className="btn-label">Label</p>
+              <Button variant="primary-white" type="label" onClick={() => console.log("clicked")}>
+                Submit
+              </Button>
+            </span>
+
+            <span className="btn-container">
+              <p className="btn-label">Disabled</p>
+              <Button variant="primary-white" type="label" disabled>
+                Submit
+              </Button>
+            </span>
+
+            <span className="btn-container">
+              <p className="btn-label">Label Icon</p>
+              <Button variant="primary-white" type="label-icon">
+                <LuPlus/>
+                Add Item
+              </Button>
+            </span>
+          </div>
+
+          <div>
+            <span className="btn-container">
+              <p className="btn-label">Icon</p>
+              <Button variant="primary-white" type="icon">
+                <LuPlus/>
+              </Button>
+            </span>
+
+            <span className="btn-container">
+              <p className="btn-label">Icon Circle</p>
+              <Button variant="primary-white" type="icon-circle">
+                <LuPlus/>
+              </Button>
+            </span>
+
+            <span className="btn-container">
+              <p className="btn-label">Loader</p>
+              <Button variant="primary-white" type="label" loader={true}/>
+            </span>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,23 +2,30 @@ import React from "react"
 import PropTypes from 'prop-types'
 import "./button.css"
 import clsx from 'clsx'
+import {LuLoader2} from "react-icons/lu"
 
-export default function Button({children, variant, type, className, ...otherProps}) {
+export default function Button({children, loader=false, variant="primary", type="label", className, ...otherProps}) {
+
   let labelTypeClass = type === "label" ? "label-btn" : `${type}`
   let variantClass = variant && `${variant}`
+  if (loader) {
+    labelTypeClass = type === "label"? "loader label-btn" : `loader ${type}`
+    children = <LuLoader2/>
+  }
 
   const allClasses = clsx(className, variantClass, labelTypeClass, "button")
 
   return(
-    <div className={allClasses} {...otherProps}>
+    <button className={allClasses} {...otherProps}>
       {children}
-    </div>
+    </button>
   )
 }
 
 Button.propTypes = {
   children: PropTypes.elementType,
   type: PropTypes.oneOf(["label", "label-icon", "icon", "icon-circle"]),
+  loader: PropTypes.bool,
   variant: PropTypes.oneOf(["primary", "primary-white", "secondary", "secondary-white", "flat", "flat-white"]),
   className: PropTypes.oneOfType([
     PropTypes.string,
